1a71ce2ba0181c77dbd93f4095af3d98aed479f9
0xkeivin <keivincatwork@gmail.com>
2023-01-30 14:22:28 +0800

    fixed CORS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express'
 const app = express()
 const port = 5001
+const allowCrossDomain = function (req: any, res: any, next: any) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+app.use(allowCrossDomain)
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello Worlds!');
 });
 app.listen(port, () => console.log(`Running on port ${port}`))
