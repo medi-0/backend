c59dfa66c26e93c22c6f2b0c0fc5a6aa516d8724
0xkeivin <keivincatwork@gmail.com>
2023-01-30 15:57:58 +0800

    remove commented code

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,6 @@ import path from 'path'
 import pdf from "pdf-parse";
 import fs from "fs";
 import { upload, parsePdf } from './routes/upload-parse-pdf';
-// working 
-// const upload = multer({ dest: "uploads/" });
-// try with file name and extensions
-
-// creat a global variable test1
-
-// const uploadDir = 'uploads/'
-// let savedFilePath = ''
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, uploadDir)
-//     },
-//     filename: function (req, file, cb) {
-//         console.log(file)
-//         console.log(file.originalname)
-//         console.log(file.filename)
-//         const tempFilePath = Date.now() + '_' + file.originalname
-//         // save value to global variable
-//         savedFilePath = tempFilePath
-//         console.log(`savedFilePath1:${savedFilePath}`)
-//         cb(null, tempFilePath) //Appending extension
-//     }
-// })
-
-// const upload = multer({ storage: storage });
 
 const app = express()
 const port = 5001
@@ -41,57 +15,8 @@ const allowCrossDomain = function (req: any, res: any, next: any) {
 };
 app.use(allowCrossDomain)
 app.get('/', function (req, res) {
-    console.log("/ is called")
     res.send('Hello Worlds!');
 });
-// upload file
-// const uploadFiles = async (req: any, res: any) => {
-//     console.log("/uploadfile called")
-//     console.log(req.body);
-//     console.log(req.files);
-//     // const resp = upload.array("file")
-//     // console.log(`resp: ${resp}`)
-
-//     // try catch to upload file via multer
-//     try {
-//         upload.array("file")
-//         // return res.json({ message: `Successfully uploaded file to: ${uploadDir + savedFilePath} ` });
-//     } catch (error) {
-//         console.error(error)
-//         return res.status(500).send("Error uploading file");
-//     }
-//     // try to parsePDF to text 
-//     try {
-//         const currentFilePath = process.cwd() + '/' + uploadDir + savedFilePath
-//         console.log(`savedFilePath2:${savedFilePath}`)
-//         console.log(currentFilePath)
-//         const dataBuffer = fs.readFileSync(currentFilePath);
-
-//         const pdfResult = await pdf(dataBuffer).then(function (data: any): string {
-//             // number of pages
-//             console.log(data.numpages);
-//             // number of rendered pages
-//             console.log(data.numrender);
-//             // PDF info
-//             console.log(data.info);
-//             // PDF metadata
-//             console.log(data.metadata);
-//             // PDF.js version
-//             // check https://mozilla.github.io/pdf.js/getting_started/
-//             console.log(data.version);
-//             // PDF text
-//             console.log(data.text);
-
-//             return JSON.stringify(data.text);
-//         });
-//         res.status(200).json({ data: pdfResult });
-//         // res.status(200).json({ data: 'ok' });
-
-//     } catch {
-//         res.status(500).send("Error parsing pdf file")
-//     }
-// }
-// works
 app.post("/processpdf", upload.array("file"), parsePdf);
 
 // final
