384c498516da65741db720629cdb29d0530c435e
0xkeivin <keivincatwork@gmail.com>
2023-01-30 15:55:47 +0800

    upload and parse pdf route works

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,35 @@
 import express from 'express'
 import multer from 'multer'
 import path from 'path'
-
+import pdf from "pdf-parse";
+import fs from "fs";
+import { upload, parsePdf } from './routes/upload-parse-pdf';
 // working 
 // const upload = multer({ dest: "uploads/" });
 // try with file name and extensions
 
-const uploadDir = 'uploads/'
-let savedFilePath = ''
+// creat a global variable test1
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadDir)
-    },
-    filename: function (req, file, cb) {
-        console.log(file)
-        console.log(file.originalname)
-        console.log(file.filename)
-        const tempFilePath = Date.now() + '_' + file.originalname
-        // save value to global variable
-        savedFilePath = tempFilePath
-        cb(null, tempFilePath) //Appending extension
-    }
-})
+// const uploadDir = 'uploads/'
+// let savedFilePath = ''
 
-const upload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, uploadDir)
+//     },
+//     filename: function (req, file, cb) {
+//         console.log(file)
+//         console.log(file.originalname)
+//         console.log(file.filename)
+//         const tempFilePath = Date.now() + '_' + file.originalname
+//         // save value to global variable
+//         savedFilePath = tempFilePath
+//         console.log(`savedFilePath1:${savedFilePath}`)
+//         cb(null, tempFilePath) //Appending extension
+//     }
+// })
+
+// const upload = multer({ storage: storage });
 
 const app = express()
 const port = 5001
@@ -40,40 +45,54 @@ app.get('/', function (req, res) {
     res.send('Hello Worlds!');
 });
 // upload file
-const uploadFiles = (req: any, res: any) => {
-    console.log("/uploadfile called")
-    console.log(req.body);
-    console.log(req.files);
-    // const resp = upload.array("file")
-    // console.log(`resp: ${resp}`)
+// const uploadFiles = async (req: any, res: any) => {
+//     console.log("/uploadfile called")
+//     console.log(req.body);
+//     console.log(req.files);
+//     // const resp = upload.array("file")
+//     // console.log(`resp: ${resp}`)
 
-    // try catch to upload file via multer
-    try {
-        upload.array("file")
-        return res.json({ message: `Successfully uploaded file to: ${uploadDir + savedFilePath} ` });
-    } catch (error) {
-        console.error(error)
-        return res.status(500).send(error);
-    }
+//     // try catch to upload file via multer
+//     try {
+//         upload.array("file")
+//         // return res.json({ message: `Successfully uploaded file to: ${uploadDir + savedFilePath} ` });
+//     } catch (error) {
+//         console.error(error)
+//         return res.status(500).send("Error uploading file");
+//     }
+//     // try to parsePDF to text 
+//     try {
+//         const currentFilePath = process.cwd() + '/' + uploadDir + savedFilePath
+//         console.log(`savedFilePath2:${savedFilePath}`)
+//         console.log(currentFilePath)
+//         const dataBuffer = fs.readFileSync(currentFilePath);
 
+//         const pdfResult = await pdf(dataBuffer).then(function (data: any): string {
+//             // number of pages
+//             console.log(data.numpages);
+//             // number of rendered pages
+//             console.log(data.numrender);
+//             // PDF info
+//             console.log(data.info);
+//             // PDF metadata
+//             console.log(data.metadata);
+//             // PDF.js version
+//             // check https://mozilla.github.io/pdf.js/getting_started/
+//             console.log(data.version);
+//             // PDF text
+//             console.log(data.text);
 
-    // convert form data to File object
-    // const fileObj = new File([singleFile.data], singleFile.name, { type: singleFile.mimetype });
-    // console.log(fileObj);
-    // singleFile.mv(`./uploads/${singleFile.name}`, (error: any) => {
-    //     if (error) {
-    //         console.error(error);
-    //         return res.status(500).send(error);
-    //     }
-    //     res.json({ message: "Successfully uploaded files" });
-    // });
-}
-// app.post("/uploadfile", uploadFiles);
-// app.post("/uploadfile", upload.array("files"), uploadFiles);
+//             return JSON.stringify(data.text);
+//         });
+//         res.status(200).json({ data: pdfResult });
+//         // res.status(200).json({ data: 'ok' });
+
+//     } catch {
+//         res.status(500).send("Error parsing pdf file")
+//     }
+// }
 // works
-app.post("/uploadfile", upload.array("file"), uploadFiles);
-// test single handler
-app.post("/uploadfile", uploadFiles);
+app.post("/processpdf", upload.array("file"), parsePdf);
 
 // final
 app.listen(port, () => console.log(`Running on port ${port}`))
